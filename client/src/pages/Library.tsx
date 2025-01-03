4b1ee599f7b059f4ab5210ca38c7689dc7a388a7
dxtym <dilmurod.abdusamadov2004@gmail.com>
2025-01-03 16:29:37 +0500

    fix: linter error

--- client/src/pages/Library.tsx
+++ client/src/pages/Library.tsx
@@ -1,29 +1,25 @@
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
-import axios from "axios";
-import { useEffect, useState } from "react";
 import Empty from "@/components/Empty";
-import WebApp from "@twa-dev/sdk";
-import { IManga } from "@/types";
 
 export default function Library() {
-  const url = import.meta.env.VITE_API_URL;
-  const [data, setData] = useState<Array<IManga>>([]);
+  // const url = import.meta.env.VITE_API_URL;
+  // const [data, setData] = useState<Array<IManga>>([]);
 
-  useEffect(() => {
-    axios
-      .get(`${url}/library`, {
-        headers: { authorization: `tma ${WebApp.initData}` },
-      })
-      .then((res) => {
-        setData(res.data);
-        if (!res.data || res.data.length == 0) {
-          document.body.style.height = "100vh";
-          document.body.style.overflow = "hidden";
-        }
-      })
-      .catch((err) => console.error(err));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get(`${url}/library`, {
+  //       headers: { authorization: `tma ${WebApp.initData}` },
+  //     })
+  //     .then((res) => {
+  //       setData(res.data);
+  //       if (!res.data || res.data.length == 0) {
+  //         document.body.style.height = "100vh";
+  //         document.body.style.overflow = "hidden";
+  //       }
+  //     })
+  //     .catch((err) => console.error(err));
+  // }, []);
 
   return (
     <>
